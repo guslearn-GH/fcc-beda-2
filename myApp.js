750dc21b6fc753259dfed18f5e78075dd3e6f5fe
Gus <56235860+guslearn-GH@users.noreply.github.com>
2025-04-19 19:55:57 +0000

    #

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 //console.log("Hello World");
@@ -7,6 +8,12 @@ app.use("/public", express.static(path));
 app.get("/", function (req, res) {
   //res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/json", function (req, res) {
+  let result = "";
+  if (process.env.MESSAGE_STYLE == "uppercase") result = "HELLO JSON";
+  else result = process.env.MESSAGE_STYLE; //"hello json";
+  res.json({ message: result });
 });
 
 module.exports = app;
