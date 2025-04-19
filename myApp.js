599822d8b3c33010f84a321a689c1cc4bb5d34c9
Gus <56235860+guslearn-GH@users.noreply.github.com>
2025-04-19 19:41:16 +0000

    #

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 let express = require("express");
 let app = express();
 //console.log("Hello World");
+let path = __dirname + "/public";
+app.use("/public", express.static(path));
 
 app.get("/", function (req, res) {
   //res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
 });
-
-//app.use(__dirname + "/public", express.static());
 
 module.exports = app;
